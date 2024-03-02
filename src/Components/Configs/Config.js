92006854285b62dfcdbacb0618702c73f8856d89
abhay sakariya <abhay.work233@gmail.com>
2024-03-02 11:57:05 +0530

    change in config url

--- src/Components/Configs/Config.js
+++ src/Components/Configs/Config.js
@@ -1,6 +1,6 @@
 const URL_CONFIG = {
-    BASE_URL : "http://localhost:3000", // Local Development Url
-    // BASE_URL : "https://blog-backend-rhvw.onrender.com", // Hosted url
+    // BASE_URL : "http://localhost:3000", // Local Development Url
+    BASE_URL : "https://blog-backend-rhvw.onrender.com", // Hosted url
 };
 
 export default URL_CONFIG;
